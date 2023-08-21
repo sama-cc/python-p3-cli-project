e888157df3b2a695edab08b0055e55f60a17e249
Sam A Carpenter-Camhi <sam.camhi@gmail.com>
2023-08-21 00:58:42 -0400

    added user, game, and owned_game tables

---
+++ lib/db/models.py
@@ -0,0 +1,49 @@
+from sqlalchemy import func
+from sqlalchemy import Column, Integer, String, DateTime, Table, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+owned_game = Table(
+    'owned_game',
+    Base.metadata,
+    Column('game_id', ForeignKey('games.id'), primary_key=True),
+    Column('user_id', ForeignKey('users.id'), primary_key=True),
+    extend_existing=True,
+)
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer(), primary_key=True)
+    username = Column(String(), unique=True, nullable=False)
+    email = Column(String(), unique=True)
+    region = Column(String())
+    games = relationship('Game', secondary=owned_game, back_populates='users')
+    created_at = Column(DateTime(), server_default=func.now())
+    updated_at = Column(DateTime(), onupdate=func.now())
+
+    def __repr__(self):
+        return f'User(id={self.id}, ' + \
+            f'username="{self.username}", ' + \
+            f'email="{self.email}", ' + \
+            f'user_since="{self.created_at}", ' + \
+            f'region="{self.region})"'
+
+class Game(Base):
+    __tablename__ = 'games'
+
+    id = Column(Integer(), primary_key=True)
+    title = Column(String())
+    genre = Column(String())
+    platform = Column(String())
+    price = Column(Integer())
+    users = relationship('User', secondary=owned_game, back_populates='games')
+    created_at = Column(DateTime(), server_default=func.now())
+    updated_at = Column(DateTime(), onupdate=func.now())
+
+    def __repr__(self):
+        return f'Game(id={self.id}, ' + \
+            f'title="{self.title}", ' + \
+            f'platform="{self.platform})"'
